773399f9928f6e1167f62710e5fee83d9355f8de
magas.akos@gmail.com <magas.akos@gmail.com@cd52b727-4014-fee8-f77f-860f097b58c6>
2013-01-18 16:12:12 +0000

    tagfelvétel kész listázás kész könyvfelvétel kész

--- test/Konyv.cpp
+++ test/Konyv.cpp
@@ -11,15 +11,25 @@
 using namespace std;
 
 int Books::nextId = 1; //az elso könyv id-je.
+int Books::_sz_len = 0; //az elso könyv id-je.
+int Books::_k_len = 0; //az elso könyv id-je.
+int Books::_c_len = 0; //az elso könyv id-je.
+
+
 
 Books::Books(std::string szerzo, std::string cim, std::string kiado, int evszam, int kiadas, int isbn) :  _szerzo(szerzo), _cim(cim), _kiado(kiado), _evszam(evszam), _kiadas(kiadas), _isbn(isbn)
 {
     _szabad = true;
     _id = ++nextId;
     _datum = "0"; //nincs kölcsönözve
+    _sz_len = (szerzo.size()>_sz_len && szerzo.size()<=40)? szerzo.size():_sz_len;
+    _k_len = (kiado.size()>_k_len && kiado.size()<=40)? kiado.size():_k_len;
+    _c_len = (cim.size()>_c_len && cim.size()<=40)? cim.size():_c_len;
 }
-Books::Books(int id, std::string szerzo, std::string cim, std::string kiado, int evszam, int kiadas, int isbn) : _id(id), _szerzo(szerzo), _cim(cim), _kiado(kiado), _evszam(evszam), _kiadas(kiadas), _isbn(isbn)
+Books::Books(int id, std::string szerzo, std::string cim, std::string kiado, int evszam, int kiadas, int isbn)
 {
+    *this = Books(szerzo, cim, kiado, evszam, kiadas, isbn);
+    _id = id;
     if (id>nextId)
     {
         nextId=id;
@@ -38,16 +48,27 @@ void Books::list()
 //cout<< "ID\t" <<  "SZERZO\t" <<  "CIM\t" <<  "KIADO\t" <<  "EVSZAM\t" << "KIADAS\t" <<  "ISBN"<< endl;
 string kol = (_szabad)? "igen":"nem";
     cout<< _id << "\t" <<  _szerzo;
-    Space(30-_szerzo.size(), " ");
+    Space(_sz_len-_szerzo.size()+2, " ");
     cout <<  _cim;
-    Space(40-_cim.size(), " ");
+    Space(_c_len-_cim.size()+2, " ");
     cout <<  _kiado;
-    Space(30-_kiado.size(), " ");
+    Space(_k_len-_kiado.size()+2, " ");
     cout  <<  _evszam << "\t" <<  _kiadas << ".\t" <<  _isbn << "\t " << kol;
     if (_ki != 0){cout << " (" << _ki->GetNev() << ", id=" << _ki->GetId() <<")";}
     cout << endl;
 }
+void Books::list_f(){
+    cout<< "ID\t" <<  "SZERZO";
+    Space(_sz_len-4, " ");
+    cout <<  "CÍM";
+    Space(_c_len-1, " ");
+    cout <<  "KIADÓ";
+    Space(_k_len-4, " ");
+    cout <<  "ÉVSZÁM\t" << "KIADÁS\t" <<  "ISBN\t\t"<< "KÖLCSÖNÖZHETÖ" <<  endl;
+    Space(130, "-");
+    cout << endl;
 
+}
 
 /**
  * könyv szerkesztése
